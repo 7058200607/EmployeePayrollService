890aad35c88abea14408eb27799deaf17d2055e9
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-18 12:21:01 +0530

    add gender add insert table into database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@
 create database Employee_service;
 ---UC2
 use Employee_service;
-create table emp_payroll2(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date);
+create table emp_payroll2(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date,gender varchar(200));
 select*from emp_payroll2;
 ---UC3
 insert into emp_payroll2 values('komal',20000,'2021-06-17');
@@ -17,3 +17,7 @@ select *from emp_payroll2;
 ---UC5
 select *from emp_payroll2 where empname='punam';
 select *from emp_payroll2 where  start_date between CAST('2019-03-01' as  date)and GETDATE();
+---UC6
+alter table emp_payroll2 add gender varchar(200);
+update emp_payroll2 set gender ='F'where empname='komal' or empname='mrunal'or empname='punam'or empname='aayushi';
+update emp_payroll2 set gender='M'where empname='sarthak'or empname='saurav';
