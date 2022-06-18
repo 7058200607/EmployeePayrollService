1a8fdf56342f5d98dd5133e352f40898418614cb
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-18 12:32:59 +0530

    ability to extend details into database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@
 create database Employee_service;
 ---UC2
 use Employee_service;
-create table emp_payroll2(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date,gender varchar(200));
+create table emp_payroll2(empid int identity(1,1)primary key,empname varchar(200),salary bigint,start_date date,gender varchar(200),phoneNumber bigInt,departement varchar(200),address varchar(200));
 select*from emp_payroll2;
 ---UC3
 insert into emp_payroll2 values('komal',20000,'2021-06-17');
@@ -32,3 +32,12 @@ select MAX(salary) as MaximumSalary from emp_payroll2 where gender ='F'GROUP BY 
 select MAX(salary) as MaximumSalary from emp_payroll2 where gender ='M'GROUP BY gender;
 select COUNT(salary) as CountSalary from emp_payroll2 where gender ='F'GROUP BY gender;
 select COUNT(salary) as AvarageSalary from emp_payroll2 where gender ='M'GROUP BY gender;
+---UC8
+alter table emp_payroll2 add phoneNumber bigInt,departement varchar(200) not null default 'developer',address varchar(200) default'pune';
+update emp_payroll2 set phoneNumber=9049784686,departement='testing',address='pune'where empname='komal';
+update emp_payroll2 set phoneNumber=8483947685,departement='developer',address='mumbai'where empname='mrunal';
+update emp_payroll2 set phoneNumber=9049788723,departement='manager',address='delhi'where empname='punam';
+update emp_payroll2 set phoneNumber=8749778226,departement='testing',address='pune'where empname='aayushi';
+update emp_payroll2 set phoneNumber=9787658421,departement='Developer',address='mumbai'where empname='sarthak';
+update emp_payroll2 set phoneNumber=7049784686,departement='Developer',address='pune'where empname='saurav';
+select*from emp_payroll2;
